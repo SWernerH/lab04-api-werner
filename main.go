576a2d7943787af94d8ff3a7ba31f8162ab3df1d
WernerSosa <2024161143@ub.edu.bz>
2026-02-21 19:18:16 -0600

    Complete part 2

--- main.go
+++ main.go
@@ -18,3 +18,20 @@ func (rw *responseWriter) WriteHeader(code int) {
 	rw.statusCode = code
 	rw.ResponseWriter.WriteHeader(code)
 }
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		rw := &responseWriter{w, http.StatusOK}
+
+		next.ServeHTTP(rw, r)
+
+		log.Printf("%s %s %d %v",
+			r.Method,
+			r.URL.Path,
+			rw.statusCode,
+			time.Since(start),
+		)
+	})
+}
