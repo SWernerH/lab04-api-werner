082fcc33bb7e53417d96bf48f37dd85a6d6f1f42
WernerSosa <2024161143@ub.edu.bz>
2026-02-21 19:24:03 -0600

    Complete part-4

--- main.go
+++ main.go
@@ -39,3 +39,34 @@ func loggingMiddleware(next http.Handler) http.Handler {
 type application struct {
 	logger *slog.Logger
 }
+
+func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "status: available\n")
+	app.logger.Info("healthcheck handler called")
+}
+
+func (app *application) listBooks(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "list of books (coming soon)\n")
+	app.logger.Info("listBooks handler called")
+}
+
+func (app *application) getBook(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "get book with id: %s\n", id)
+	app.logger.Info("getBook handler called", "id", id)
+}
+
+func (app *application) createBook(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "book created (coming soon)\n")
+	app.logger.Info("createBook handler called")
+}
+
+func (app *application) deleteBook(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	w.WriteHeader(http.StatusNoContent)
+	app.logger.Info("deleteBook handler called", "id", id)
+}
